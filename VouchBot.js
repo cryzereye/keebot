772ef24cc728e32351a5c826fec0b3a9de6c88f8
Jericho Francis Carlos <cryzereye@gmail.com>
2022-08-15 14:18:18 +0800

    initial extract complete + connection errors tbf

--- VouchBot.js
+++ VouchBot.js
@@ -1,5 +1,5 @@
-const { Client, Intents } = require('discord.js');
-const { discord_token, command_sign } = require('./json/config.json');
+const { Client, Intents, TextChannel } = require('discord.js');
+const { discord_token, command_sign, me_id, targetCHID, botCHID } = require('./json/config.json');
 const { Scorer } = require('./Scorer');
 
 class VouchBot{
@@ -11,6 +11,8 @@ class VouchBot{
         Intents.FLAGS.GUILD_MESSAGES, // required daw
       ]
     });
+    this.client.login(discord_token);
+    let vouchChannel = this.client.channels.cache.get(targetCHID);
 
     // events detection
     this.client.on('ready', () => {
@@ -21,12 +23,51 @@ class VouchBot{
       let msg = '';
       let authorID = message.author.id.toString();
       let authorName = message.author.username + '#' + message.author.discriminator;
+      let messageCHID = message.channel.id;
       if( authorID === this.client.user.id ) return; // if bot sent the message, ignore
-      if( message.content.startsWith(command_sign + 'stats')) {
+      if( message.content.startsWith(command_sign + 'stats') && messageCHID == botCHID) {
         msg = this.scorer.getStats(authorID);
         message.reply(msg);
       }
+      else if( message.content.startsWith(command_sign + 'extract')){
+        console.log('Checking if admin...');
+        if(authorID == me_id){ // commands from admin/me
+          console.log('Data extraction from #verify-transactions starting...');
+          let count = 0;
+          let hasMoreMessages = true;
+          let lastMessageID = "1008351176410026097";
+          console.log(vouchChannel);
+          while(hasMoreMessages) {
+            vouchChannel.messages.fetch({ limit: 100, before: lastMessageID }).then(msglist => {
+              let owner;
+              msglist.forEach(msg =>{
+                try {
+                  owner = msg.author.username + '#' + msg.author.discriminator;
+                  let mentions = msg.mentions.users; // mentioned by initial vouch
+                  console.log('Extracting data for ' + owner);
+                  mentions.map(x => {
+                    this.scorer.addPoint(msg.author.id.toString(), msg.author.username + '#' + msg.author.discriminator, x.username + '#' + x.discriminator);
+                  });
+                }
+                catch(e){
+                  console.log('Error with extracting data for ' + owner);
+                  console.log(e);
+                }
+                count++;
+                if(count % 100 != 0){
+                  hasMoreMessages = false;
+                  lastMessageID = msg.id;
+                }
+              });
+            })
+            .catch(console.error);
+            console.log('Message count: ' + count);
+          }
+        }
+      }
       else {
+        // only for vouch channel
+        if(messageCHID != targetCHID) return;
         // process all verifications
         // id1 sender, id2 mentioned
 
@@ -44,7 +85,6 @@ class VouchBot{
         }
       }
     });
-    this.client.login(discord_token);
   }
 }
 
