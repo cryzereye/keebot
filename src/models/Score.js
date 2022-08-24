9f790d81b2c519e0f9cb30e908cd599fc55b2c2d
Jericho Francis Carlos <cryzereye@gmail.com>
2022-08-24 23:59:20 +0800

    initial mongodb equivalent of score updates. NOT TESTED

--- src/models/Score.js
+++ src/models/Score.js
@@ -1,3 +1,78 @@
-class Score{
-    constructor(){}
+/**
+ * holds all functions directly accessing the database
+ */
+class Score {
+  constructor() { }
+
+  /**
+   * adds a point to the given user + its transaction target
+   * @param {Collection} [db] collection instance
+   * @param {int} [id] discord ID
+   * @param {string} [username] discord username
+   * @param {string} [target] username of targer discord user
+   */
+  addPoint(db, id, name, target) {
+    let record = db.find({ _id : id});
+    if(record != null){
+      let trans = record.transactions.find({username : target});
+      if(trans != null){
+        record.transactions.updateOne(
+          {username : target},
+          {points : trans.points + 1}
+        );
+        record.updateOne(
+          { _id : id},
+          {points : record.points + 1}
+        );
+      }
+    }
+    else
+      db.insertOne(this.newRecord(id, name, target));
+  }
+
+  /**
+   * will return a JSON object for a new insert
+   * @param {Collection} [db] collection instance
+   * @param {int} [id] discord ID
+   * @param {string} [name] discord username
+   * @param {string} [target] username of targer discord user
+   */
+  newRecord(id, name, target) {
+    let str = `{
+      "_id": ${id},
+      "username": ${name},
+      "points": 1,
+      "transactions": [{
+        "username": ${target},
+        "points": 1
+      }]`;
+
+      return JSON.parse(str);
+  }
+
+  /**
+   * returns the score of the given user's id
+   * @param {MongoClient.<collection>} [db] collection instance
+   * @param {number} [id] discord ID
+   * @returns {number}
+   */
+  getScore(db, id) {
+    let record = db.find({ _id: id });
+    if(record != null){
+      return record.points;
+    }
+    return null;
+  }
+
+  /**
+   * returns the JSON object of the user given its id
+   * @param {MongoClient.<collection>} [db] collection instance
+   * @param {number} [id] discord ID
+   * @returns {Object{}}
+   */
+  findRecord(db, id){
+    return db.find({ _id: id });
+  }
 }
+
+module.exports = { Score }
--- src/util/DBManager.js
+++ src/util/DBManager.js
@@ -1,10 +1,33 @@
 const { MongoClient } = require('mongodb');
-const { connURI} = require('../json/config.json');
+const { Score } = require('../models/Score');
+const { Config } = require('../models/Config');
+const { connURI } = require('../json/config.json');
+
 class DBManager {
-    constructor(){
-        this.dbclient = new MongoClient(connURI);
-        if(await client.connect()){
-            console.log("MongoDB connected!");
-        }
+  constructor() {
+    this.dbclient = new MongoClient(connURI);
+    this.score = this.dbclient.score;
+    this.config = this.dbclient.config;
+
+    if (await client.connect()) {
+      console.log("MongoDB connected!");
     }
+  }
+
+  /**
+   * adds a point to the given id
+   * @type {null}
+   * @param {int} [id] discord ID
+   * @param {string} [username] discord username
+   * @param {string} [target] username of targer discord user
+   */
+  addScore(id, username, target) {
+    Score.addpoint(this.score, id, username, target);
+  }
+
+  getScore(id) {
+    Score.getScore(this.score, id);
+  }
 }
+
+module.exports = { DBManager }
