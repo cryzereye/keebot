7c8b8e930f6d4766d6aa03561c6e6b96cc33b1ec
Jericho Francis Carlos <cryzereye@gmail.com>
2022-08-25 12:46:37 +0800

    DBManager successful connection

--- src/util/DBManager.js
+++ src/util/DBManager.js
@@ -1,17 +1,21 @@
 const { MongoClient } = require('mongodb');
 const { Score } = require('../models/Score');
 const { Config } = require('../models/Config');
-const { connURI } = require('../json/config.json');
+const { connURI, dbname, collnames } = require('../json/config.json');
 
 class DBManager {
   constructor() {
-    this.dbclient = new MongoClient(connURI);
-    this.score = this.dbclient.score;
-    this.config = this.dbclient.config;
+    const dbclient = new MongoClient(connURI);
 
-    if (await client.connect()) {
-      console.log("MongoDB connected!");
-    }
+    console.log("Connecting to database...");
+    dbclient.connect((err, db) => {
+      this.scoredb = db.db(dbname).collection(collnames[0]);
+      if(this.scoredb !== undefined)
+        this.score = new Score();
+
+      console.log("Connected to database!");
+      dbclient.close();
+    });
   }
 
   /**
@@ -22,11 +26,11 @@ class DBManager {
    * @param {string} [target] username of targer discord user
    */
   addScore(id, username, target) {
-    Score.addpoint(this.score, id, username, target);
+    this.score.addpoint(this.score, id, username, target);
   }
 
   getScore(id) {
-    Score.getScore(this.score, id);
+    this.score.getScore(this.score, id);
   }
 }
 
