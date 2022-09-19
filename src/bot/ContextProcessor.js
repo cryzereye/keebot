3a842368caf8d0ef3cdc381526c3e9544a291d13
Jericho Francis Carlos <cryzereye@gmail.com>
2022-09-20 02:08:07 +0800

    fix(post): fixes context menu calls for post functions

--- src/bot/ContextProcessor.js
+++ src/bot/ContextProcessor.js
@@ -1,16 +1,27 @@
-const { admins } = require('../json/config.json');
 const { commands } = require('../globals/commands.json');
+const dUtil = require('../util/DiscordUtil');
+
 class ContextProcessor {
   constructor() { }
 
   async processContext(interaction, postmngr, reportmngr) {
     const { commandName, targetId } = interaction;
     switch (commandName) {
-      case commands[5].name: return await postmngr.editPostModal(interaction, targetId);
-      case commands[6].name: return await postmngr.deletePostModal(interaction, targetId);
-      case commands[7].name: return await postmngr.soldPostModal(interaction, targetId);
-      case commands[8].name: return await reportmngr.reportPost(interaction, targetId);
+      case commands[5].name: this.processResults(interaction, await postmngr.editPostModal(interaction, targetId)); break;
+      case commands[6].name: this.processResults(interaction, await postmngr.deletePostModal(interaction, targetId)); break;
+      case commands[7].name: this.processResults(interaction, await postmngr.soldPostModal(interaction, targetId)); break;
+      case commands[8].name: this.processResults(interaction, await reportmngr.reportPost(interaction, targetId)); break;
     }
+  }
+
+  /**
+ * does the results processing for all modal functions
+ * @param {discord.js.Interaction} interaction 
+ * @param {Object} data 
+ */
+  processResults(interaction, data) {
+    const { success, content, isModal, modal } = data;
+    dUtil.postProcess(interaction, success, content, isModal, modal);
   }
 }
 
