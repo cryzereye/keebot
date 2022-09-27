81b1886dd29704c7727e8cdf4f96c88cc7b54bff
Jericho Francis Carlos <cryzereye@gmail.com>
2022-09-27 09:58:23 +0800

    feat(config): adds validator utils

---
+++ src_ts/config/utils/Utilities.ts
@@ -0,0 +1,15 @@
+const numRegex = new RegExp(/^\d+&/);
+const channelNameRegex = new RegExp(/^(\d|\w|_)(\d|\w|_|-){0-99}$/);  // starts with /w /d or _. /w /d - _ only. 100 chars max. no consecutive -
+const roleNameRegex = new RegExp(/^.{1-100}$/);
+
+export function validID (id: string): boolean {
+    return numRegex.test(id);
+} 
+
+export function validChannelName (ch: string): boolean {
+    return channelNameRegex.test(ch);
+} 
+
+export function validroleName (role: string):boolean {
+    return roleNameRegex.test(role);
+} 
